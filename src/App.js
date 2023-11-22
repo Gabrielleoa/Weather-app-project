00bf5d986704889c39dd81709e5f930289c227b4
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-11-22 08:59:10 +0300

    display ten jokes and more

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Displaymore from './components/DisplayMore';
 
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Randomjoke from './components/RandomJoke';
+import Footer from './components/Footer';
 
 function App() {
   const [jokes, setJokes] = useState([]);
+
 
   useEffect(() => {
     fetch('https://official-joke-api.appspot.com/jokes/ten')
@@ -18,36 +20,25 @@ function App() {
       });
   }, []);
 
-  const fetchNewRandomJokes = () => {
-    fetch('https://official-joke-api.appspot.com/jokes/random')
-      .then(response => response.json())
-      .then(data => {
-        setJokes(data);
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error fetching new random jokes:', error);
-      });
-  };
-
   return (
     <div>
       <h1>Chuckle Charm</h1>
       <p>Welcome to the World Of Fun and Comedy. Crack your ribs with laughter</p>
-      <div>
-        {jokes.map((joke, index) => (
-          <div key={index}>
-              <p>Setup: {joke.setup}</p>
-              <p>Punchline: {joke.punchline}</p>
-            <hr></hr>
-          </div>
+      <ol>
+        
+          {jokes.map((joke, index) => (
+          <li key={index}>
+            <p>Setup: {joke.setup}</p>
+            <p>Punchline: {joke.punchline}</p>
+            <hr />
+          </li>
         ))}
-      </div>
-      <Displaymore fetchNewRandomJokes={fetchNewRandomJokes} />
+       
+      </ol>
+    <Randomjoke />
+    <Footer />
     </div>
   );
 }
-
-
 
 export default App;
